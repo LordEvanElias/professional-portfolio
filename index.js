88924f7fe59e0fb132ac6b2d703df409cdf15f12
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-02-17 13:11:11 -0600

    Finished the styling of the nav bar and started coding the javascript event listeners.

---
+++ index.js
@@ -0,0 +1,16 @@
+// const goToLinkedin = {
+//     window.location.replace.href=("https://www.linkedin.com/in/evan-elias-8515a810b/")
+// };
+
+// const goToGithub = {
+//     window.location.replace.href=("https://github.com/LordEvanElias")
+// };
+
+// document.querySelector(".github-logo").addEventListener("click", goToGithub);
+// document.querySelector(".bikes4u").addEventListener("click", goToBikes4U);
+// document.querySelector(".weather-thingy").addEventListener("click", goToWeatherThingy);
+// document.querySelector(".README-maker").addEventListener("click", goToREADMEMaker);
+// document.querySelector(".password-generator").addEventListener("click", goToPasswordGenerator);
+// document.querySelector(".team-profile-maker").addEventListener("click", goToTeamProfileMaker);
+// document.querySelector(".c2ez-pos").addEventListener("click", goToC2ezPOS);
+// document.querySelector(".linkedin-logo").addEventListener("click", goToLinkedin);
